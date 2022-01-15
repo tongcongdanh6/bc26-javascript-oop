719675a596b99694e0e3bde91e111a2943faa8fc
tongcongdanh6 <tong.congdanh@gmail.com>
2022-01-15 20:42:13 +0700

    version 204214012022

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+var arrMonAn = [
+    {maMonAn:1,tenMonAn:'Nước lẩu haidilao',giaTien:100},
+    {maMonAn:2,tenMonAn:'Mì cay thành đô',giaTien:200},
+    {maMonAn:3,tenMonAn:'Mực bạch ngọc',giaTien:300},
+]
